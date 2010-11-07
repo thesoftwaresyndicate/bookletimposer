d86cf94cd5f4d9a5b17a7bd6bcf5858ccbb6af47
Kjö Hansi Glaz <kjo@a4nancy.net.eu.org>
2010-11-07 03:38:41 +0100

    Add persistent preferences to the backend

--- src/bookletbinder/backend.py
+++ src/bookletbinder/backend.py
@@ -30,6 +30,7 @@
 ########################################################################
 
 import pypdfconv
+import os.path
 
 class ConversionType:
     """The conversion type constants"""
@@ -40,13 +41,101 @@ class ConversionType:
     REDUCE = 3
     """The conversion from multiple input pages to one output page"""
 
-class PersistentConverter(pypdfconv.FileConverter):
-    """
-    This class performs conversions on true files.
-    """
+class ConverterPreferences(object):
+    def __init__(self):
+        self._infile_name = None
+        self._conversion_type = None
+        self.copy_pages = None
+        self.layout = None
+        self.paper_format = None
+        self.paper_orientation = None
+        self.outfile_name = None
 
-    XXX: Find a better name
+    @property
+    def infile_name(self):
+        return self._infile_name
 
+    @infile_name.setter
+    def infile_name(self, value):
+        assert value == None or os.path.isfile(value)
+        self._infile_name = value
+
+    @property
+    def conversion_type(self):
+        return self._conversion_type
+
+    @conversion_type.setter
+    def conversion_type(self, value):
+        assert value == None or \
+               value == ConversionType.BOOKLETIZE or \
+               value == ConversionType.LINEARIZE or \
+               value == ConversionType.REDUCE
+        self._conversion_type = value
+
+    @property
+    def copy_pages(self):
+        return self._copy_pages
+
+    @copy_pages.setter
+    def copy_pages(self, value):
+        self._copy_pages = bool(value)
+
+    @property
+    def layout(self):
+        return self._layout
+
+    @layout.setter
+    def layout(self, value):
+        # XXX : verify value
+        self._layout = value
+
+    @property
+    def paper_format(self):
+        return self._paper_format
+
+    @paper_format.setter
+    def paper_format(self, value):
+        # XXX : verify value
+        self._paper_format = value
+
+    @property
+    def paper_orientation(self):
+        return self._paper_orientation
+
+    @paper_orientation.setter
+    def paper_orientation(self, value):
+        # XXX : verify value
+        self._paper_orientation = value
+
+    @property
+    def outfile_name(self):
+        return self._outfile_name
+
+    @outfile_name.setter
+    def outfile_name(self, value):
+        assert value == None or os.path.exists(os.path.dirname(value))
+        self._outfile_name = value
+
+    def create_converter(self):
+        if not self._infile_name:
+            # Use a better exception
+            raise Exception, "Cannot create converter without an input file"
+            return None
+        elif self._outfile_name:
+            converter = TypedFileConverter(self._infile_name, self._outfile_name)
+        else:
+            converter = TypedFileConverter(self._infile_name)
+        if self._layout: converter.set_layout(self._layout)
+        if self._paper_format: converter.set_output_format(self._paper_format)
+        if self._paper_orientation:
+            converter.set_output_orientation(self._paper_orientation)
+        if self._copy_pages: converter.set_copy_pages(self._copy_pages)
+        return converter
+
+class TypedFileConverter(pypdfconv.FileConverter):
+    """A FileConverter that stores the conversion type.
+
+    """
     def __init__(self,
                  infile_name=None,
                  outfile_name=None,
@@ -54,8 +143,7 @@ class PersistentConverter(pypdfconv.FileConverter):
                  layout='2x1',
                  format='A4',
                  copy_pages=False):
-        """
-        Create a PersistentConverter.
+        """Create a TypedFileConverter.
 
         :Parameters:
           - `infile_name`: The name to the input PDF file.
@@ -71,16 +159,16 @@ class PersistentConverter(pypdfconv.FileConverter):
             to fill the corresponding output page or not (see
             set_copy_pages).
         """
-        #Should create the FileConverter if infile_name is set
-        FileConverter.__init__(self, infile_name, outfile_name,
-                                 conversion_type, layout, format, copy_pages)
+        
+        pypdfconv.FileConverter.__init__(self, infile_name, outfile_name,
+                                         layout, format, copy_pages)
+        self._conversion_type = conversion_type
 
     # CONVERSION FUNCTIONS
     # ====================
 
     def run(self):
-        """
-        Perform the actual conversion.
+        """Perform the actual conversion.
 
         This method launches the actual conversion, using the parameters set
         before.
@@ -96,14 +184,15 @@ class PersistentConverter(pypdfconv.FileConverter):
     # ===========================
 
     def set_conversion_type(self, type):
-        """
-        Set conversion that will be performed when caling run().
+        """Set conversion that will be performed when caling run().
 
         :Parameters:
           - `type`: A constant from the ConversionType class.
         """
-        assert(type == ConversionType.BOOKLETIZE or type == ConversionType.LINEARIZE or type == ConversionType.REDUCE)
-        self.__conversion_type = type
+        assert(type == ConversionType.BOOKLETIZE or \
+               type == ConversionType.LINEARIZE or \
+               type == ConversionType.REDUCE)
+        self._conversion_type = type
 
     def get_conversion_type(self):
         """
@@ -112,7 +201,6 @@ class PersistentConverter(pypdfconv.FileConverter):
         :Returns:
             A constant from ConversionType class.
         """
-        return self.__conversion_type
+        return self._conversion_type
 
-    def set_infile_name(self, name):
-        #Should (re)create the converter
+
