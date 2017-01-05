8154fe45a7da90c1346a83ae6b3e118f69837d61
Kjö Hansi Glaz <kjo@a4nancy.net.eu.org>
2017-01-05 22:31:38 +0100

    Update website build script

--- scripts/gensite.sh
+++ scripts/gensite.sh
@@ -1,38 +1,41 @@
 #!/bin/sh
+mkdir -p build/site
+echo copying images
+cp site/img build/site/ -r
 echo generate main page
-pandoc --output site/index.html \
+pandoc --output build/site/index.html \
     --include-before-body site/inc_title.html \
     --css style.css \
     --standalone \
     --table-of-contents \
     site/index.mdwn
 echo generate changelog page
-pandoc --output site/changelog.html \
+pandoc --output build/site/changelog.html \
     --include-before-body site/inc_title.html \
     --css style.css \
     --standalone \
     CHANGELOG
 echo generate todo page
-pandoc --output site/todo.html \
+pandoc --output build/site/todo.html \
     --css style.css \
     --include-before-body site/inc_title.html \
     --standalone \
     TODO
 echo generate man page
-pandoc --output=site/bookletimposer.1.html \
+pandoc --output=build/site/bookletimposer.1.html \
     --css style.css \
     --include-before-body site/inc_title.html \
     --standalone \
     doc/bookletimposer.1.mdwn
 echo generate online help
-xsltproc -o site/help.html \
+xsltproc -o build/site/help.html \
     --stringparam html.stylesheet style.css \
     /usr/share/xml/docbook/stylesheet/nwalsh/xhtml/docbook.xsl \
-    help/C/bookletimposer.xml
+    help/C/index.docbook
 cp -r help/C/figures site/figures
 echo generate API documentation
 epydoc --docformat=restructuredtext \
-    --output=site/api/ \
+    --output=build/site/api/ \
     --inheritance=included \
     --no-private \
     --no-frames \
